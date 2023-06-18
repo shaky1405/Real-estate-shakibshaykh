614edf7f9b9885e2a5a529ae9015e82a58d191a4
shaky1405 <shakibshaikh1405@gmail.com>
2023-06-18 22:38:55 +0530

    successful
    updated

---
+++ src/Components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import react from "react";
+import "./Companies.css"
+
+const Companies = () => {
+    return(
+    <section className="c-wrapper">
+        <div className="paddings innerWidth flexCenter c-container">
+            <img src="./prologis.png" alt="" />
+            <img src="./tower.png" alt="" />
+            <img src="./equinix.png" alt="" />
+            <img src="./realty.png" alt="" />
+        </div>
+    </section>
+    );
+};
+
+export default Companies;
---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "We are currently offering the best interest rate on the market, providing customers with an excellent opportunity to earn higher returns on their investments.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Invest in our prestigious properties, where careful market analysis and strategic planning are employed to prevent unstable prices and ensure a secure and lucrative investment opportunity.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Discover the exceptional value of our properties, offering the best prices on the market, allowing you to make a wise investment that combines affordability and quality.",
+  },
+];
+export default data;
