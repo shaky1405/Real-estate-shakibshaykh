ce50cf6d727582a7fa939d2f9b12161d7c1806f1
Abhishek Mahato <57245064+foreverabhi2002@users.noreply.github.com>
2023-06-20 20:10:10 +0530

    redirecting issue resolved

--- src/Components/Companies/Companies.jsx
+++ src/Components/Companies/Companies.jsx
@@ -1,17 +1,17 @@
 import react from "react";
-import "./Companies.css"
+import "./Companies.css";
 
 const Companies = () => {
-    return(
-    <section className="c-wrapper">
-        <div className="paddings innerWidth flexCenter c-container">
-            <img src="./prologis.png" alt="" />
-            <img src="./tower.png" alt="" />
-            <img src="./equinix.png" alt="" />
-            <img src="./realty.png" alt="" />
-        </div>
+  return (
+    <section className="c-wrapper" id="companies">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="" />
+        <img src="./tower.png" alt="" />
+        <img src="./equinix.png" alt="" />
+        <img src="./realty.png" alt="" />
+      </div>
     </section>
-    );
+  );
 };
 
 export default Companies;
--- src/Components/GetStarted/GetStarted.jsx
+++ src/Components/GetStarted/GetStarted.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import './GetStarted.css'
+import "./GetStarted.css";
 
 const GetStarted = () => {
-    return (
-      <section className="g-wrapper">
-        <div className="paddings innerWidth g-container">
-            <div className="flexColCenter inner-container">
-                <span className="primaryText">Get Started with Homyz</span>
-                <span className="secondaryText">
-                    Subscribe and find super attractive price quotes from us.
-                    <br />
-                    Find your residence soon
-                </span>
-                <button className="button">
-                    <a href="mailto:shakibshaikh1405@gmail.com">Get Started</a>
-                </button>
-            </div>
+  return (
+    <section className="g-wrapper" id="getStarted">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get Started with Homyz</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us.
+            <br />
+            Find your residence soon
+          </span>
+          <button className="button">
+            <a href="mailto:shakibshaikh1405@gmail.com">Get Started</a>
+          </button>
         </div>
-      </section>
-    )
-}
-export default GetStarted
+      </div>
+    </section>
+  );
+};
+export default GetStarted;
